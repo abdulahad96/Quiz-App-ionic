66dd478ac96dcf1fc04b90e75b908f094c81688e
abdulahad96 <abdulahad199609@yahoo.com>
2018-10-10 10:36:43 +0500

    quiz App

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDCq_gZoYduUIewhlsWVM1fqDby3kfHTmI",
-    authDomain: "myproject-24ef0.firebaseapp.com",
-    databaseURL: "https://myproject-24ef0.firebaseio.com",
-    projectId: "myproject-24ef0",
-    storageBucket: "myproject-24ef0.appspot.com",
-    messagingSenderId: "83535585264"
-  };
